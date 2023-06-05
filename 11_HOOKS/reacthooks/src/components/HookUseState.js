ed35ea7c61a4390738bce9823d2a5fc43bcf1301
Maressavo <maressavargasoliveira@gmail.com>
2023-06-05 10:47:29 -0300

    useState com input

--- 11_HOOKS/reacthooks/src/components/HookUseState.js
+++ 11_HOOKS/reacthooks/src/components/HookUseState.js
@@ -10,16 +10,41 @@ const HookUseState = () => {
     userName = "Margarida Florida";
 
     setName("Jasmim Floral");
-  }
+  };
+  const [age, setAge] = useState(18);
 
-  return <div>
-    {/*1 - useState */}
-    <h2>useState</h2>
-        <p>Variável: {userName}</p>
-        <p>useState: {name}</p>
-        <button onClick={changeNames}>Mudar nomes!</button>
-    <hr />
-  </div>
+  // Criando a função handleSubmit
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+
+    // envio a uma API
+    console.log(age);
+  };
+
+  return (
+    <div>
+      {/*1 - useState */}
+      <h2>useState</h2>
+      <p>Variável: {userName}</p>
+      <p>useState: {name}</p>
+      <button onClick={changeNames}>Mudar nomes!</button>
+      {/*2 - useState e Input */}
+      <form onSubmit={handleSubmit}>
+        <input
+          type="text"
+          value={age}
+          onChange={(e) => setAge(e.target.value)}
+        />
+        <input
+            type="submit"
+            value="Enviar"
+        />
+      </form>
+      <p>Você tem {age} anos.</p>
+      <hr />
+    </div>
+  );
 };
 
 export default HookUseState;
